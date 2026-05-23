6e485f5e390f32f9b7772693123a44df1a047c83
Ravinder <rraj81482@gmail.com>
2026-05-23 10:34:04 +0530

    Change : Added Allow origin of localhost:5173

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",   # React/Vite frontend
+        "http://localhost:5173",   # React/Vite frontend
         "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
